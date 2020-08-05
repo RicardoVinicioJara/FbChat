fe992a699bb6663ab9624b5e3a4727ec459e0bdf
Ricardo Vinicio Jara jara <5940607-RicardoVinicioJara@users.noreply.gitlab.com>
2020-08-05 11:35:05 -0500

    tres en raya

--- app.py
+++ app.py
@@ -5,6 +5,64 @@ from os.path import join, dirname
 from ibm_watson import AssistantV2, LanguageTranslatorV3, TextToSpeechV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import pymysql
+from easyAI import TwoPlayersGame, Human_Player, AI_Player, Negamax
+import numpy as np
+class TresEnRaya(TwoPlayersGame):
+    def __init__(self, nplayers):
+        self.tablero = np.zeros((3, 3), dtype=int)
+        self.nplayer = 1
+        self.mapa = {}
+        cont = 1
+        for i in range(len(self.tablero)):
+            for j in range(len(self.tablero)):
+                self.mapa[cont] = (i, j)
+                cont += 1
+
+        # print(self.mapa)
+        self.players = nplayers
+
+    def possible_moves(self):
+        moves = []
+        cont = 1
+        for i in range(len(self.tablero)):
+            for j in range(len(self.tablero)):
+                if self.tablero[i][j] == 0:
+                    moves.append(cont)
+                cont += 1
+
+        # print(moves)
+        return moves
+
+    def make_move(self, casillero):
+        pos = self.mapa[casillero]
+        self.tablero[pos[0]][pos[1]] = self.nplayer
+
+    def unmake_move(self, casillero):
+        pos = self.mapa[casillero]
+        self.tablero[pos[0]][pos[1]] = 0
+
+    def lose(self):
+        for i in range(len(self.tablero)):
+            if np.all(self.tablero[i, :] == self.nopponent, axis=0) or np.all(self.tablero[:, i] == self.nopponent,
+                                                                              axis=0):
+                return True
+        if self.tablero[0][0] == self.nopponent and self.tablero[1][1] == self.nopponent and self.tablero[2][
+            2] == self.nopponent:
+            return True
+        if self.tablero[2][0] == self.nopponent and self.tablero[1][1] == self.nopponent and self.tablero[0][
+            2] == self.nopponent:
+            return True
+        return False
+
+    def show(self):
+        print(self.tablero)
+
+    def scoring(self):
+        return -100 if self.lose() else 0
+
+    def is_over(self):
+        return (self.possible_moves() == [] or self.lose())
+
 con = pymysql.connect( host="localhost", user="root", passwd="", db="preguntas")
 cursor = con.cursor(pymysql.cursors.DictCursor)
 
@@ -70,17 +128,24 @@ class EchoBot(Client):
    def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
            self.markAsDelivered(thread_id, message_object.uid)
            self.markAsRead(thread_id)
+
            if author_id != self.uid:
                messenger = message_object.text
-               print(messenger)
-               traduccion = traducir(messenger, language_translator)
-               print(traduccion)
-               respuesta = mensaje(traduccion,session)
-               print(respuesta)
-               voz(respuesta, service)
-               guardarPregunta(con,messenger,respuesta,traduccion)
-               #self.send(Message(text=respuesta), thread_id=thread_id, thread_type=thread_type)
-               self.sendLocalVoiceClips('output.mp3',Message(text=respuesta),thread_id=thread_id, thread_type=thread_type)
+               if messenger == 'inicio':
+                   self.send(Message(text="Comiensa el juego"), thread_id=thread_id, thread_type=thread_type)
+                   ai_algo = Negamax(6)
+                   tresenraya = TresEnRaya([Human_Player(), AI_Player(ai_algo)])
+                   tresenraya.play()
+               else:
+                   print(messenger)
+                   traduccion = traducir(messenger, language_translator)
+                   print(traduccion)
+                   respuesta = mensaje(traduccion,session)
+                   print(respuesta)
+                   voz(respuesta, service)
+                   guardarPregunta(con,messenger,respuesta,traduccion)
+                   #self.send(Message(text=respuesta), thread_id=thread_id, thread_type=thread_type)
+                   self.sendLocalVoiceClips('output.mp3',Message(text=respuesta),thread_id=thread_id, thread_type=thread_type)
 
 
 app = Flask(__name__)
@@ -97,6 +162,7 @@ def mensje():
 
 
 if __name__ == '__main__':
+    bandera = False;
     #app.run()
     client = EchoBot(correo, contra)
     client.listen()
